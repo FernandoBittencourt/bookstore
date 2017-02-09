49abaf420745f3fa385b751d002e41d174976534
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2017-02-09 21:51:00 -0200

    Tornando o cupom em uma entidade.

--- src/main/java/br/com/fbscript/bookstore/model/Cupom.java
+++ src/main/java/br/com/fbscript/bookstore/model/Cupom.java
@@ -2,12 +2,14 @@ package br.com.fbscript.bookstore.model;
 
 import java.util.Calendar;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Entity
 public class Cupom {
 
 	@Id
